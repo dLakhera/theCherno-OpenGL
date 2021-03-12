6ddd60dea6945bab7c54816989aa4115da5dd7eb
droidLakhera <dlakhera02@gmail.com>
2021-03-12 14:15:14 +0530

    ImGui added, v1.60

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,11 @@
 #include "vendor/glm/glm.hpp"
 #include "vendor/glm/gtc/matrix_transform.hpp"
 
+#include "vendor/imgui/imgui.h"
+#include "vendor/imgui/imgui_impl_glfw_gl3.h"
+
 // cd src
-// g++ -std=c++17 -I ./vendor/stb_image/ ./vendor/stb_image/*.cpp *.cpp -lGLEW -lGLU -lGL -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -w
+// g++ -std=c++17 -I ./vendor/stb_image/ -I ./vendor/imgui/ ./vendor/imgui/*.cpp ./vendor/stb_image/*.cpp *.cpp -lGLEW -lGLU -lGL -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -w
 // ./a.out
 
 using namespace std;
@@ -31,7 +34,7 @@ int main(void)
     if (!glfwInit())
         return -1;
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -44,18 +47,17 @@ int main(void)
     glewInit();
 
     float position[] = {
-        -0.5f, -0.5f, 0.0f, 0.0f,
-        0.5f, -0.5f, 1.0f, 0.0f,
-        0.5f, 0.5f, 1.0f, 1.0f,
-        -0.5f, 0.5f, 0.0f, 1.0f
-    };
+        100.0f, 100.5f, 0.0f, 0.0f,
+        200.5f, 100.5f, 1.0f, 0.0f,
+        200.5f, 200.5f, 1.0f, 1.0f,
+        100.5f, 200.0f, 0.0f, 1.0f};
 
     unsigned int indices[] = {
         0, 1, 2,
         2, 3, 0};
 
     GLCall(glEnable(GL_BLEND));
-    GLCall(glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
     VertexArray va;
     VertexBuffer vb(position, 4 * 4 * sizeof(float));
@@ -67,17 +69,18 @@ int main(void)
 
     IndexBuffer ib(indices, 6);
 
-    glm::mat4 proj = glm::ortho(-2.0f,2.0f,-1.5f,1.5f,-1.0f,1.0f);
+    glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+
 
     Shader shader("../res/shader/basic.shader");
     shader.Bind();
     shader.SetUniform4f("u_Color", 1.0f, 0.0f, 1.0f, 1.0f);
-    shader.SetUniformMat4f("u_MVP", proj);
 
     Texture texture("../res/textures/ladyonfire.png");
     texture.Bind();
 
-    shader.SetUniform1i("u_Texture",0);
+    shader.SetUniform1i("u_Texture", 0);
 
     va.Unbind();
     shader.Unbind();
@@ -86,18 +89,34 @@ int main(void)
 
     Renderer renderer;
 
-    float r = 0.0f;
+    ImGui::CreateContext();
+    ImGui_ImplGlfwGL3_Init(window,true);
+    ImGui::StyleColorsDark();
+
+    glm::vec3 translation(200, 200, 0);
+
+        float r = 0.0f;
     float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         renderer.Clear();
-
+        ImGui_ImplGlfwGL3_NewFrame();
         shader.Bind();
         shader.SetUniform4f("u_Color", r, 1.0f, 1.0f, 1.0f);
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+        glm::mat4 mvp = proj * view * model;
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         renderer.Draw(va, ib, shader);
+
+        {
+            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+            // ImGui::SliderFloat("Translation", 0.0f, &translation.y, 960.0f);
+            // ImGui::SliderFloat("Translation", 0.0f, 0.0f, &translation.z);
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
 
         if (r > 1.0f)
             increment = -0.05f;
@@ -105,12 +124,18 @@ int main(void)
             increment = 0.05f;
         r += increment;
 
+        ImGui::Render();
+        ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui::DestroyContext();
     glfwTerminate();
     return 0;
 }
