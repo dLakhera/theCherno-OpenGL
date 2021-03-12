ecebb95e066b07251f0274c948cafc5604138eaa
droidLakhera <dlakhera02@gmail.com>
2021-03-12 21:26:18 +0530

    Test Module implemented, working

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,8 @@
 #include "vendor/imgui/imgui.h"
 #include "vendor/imgui/imgui_impl_glfw_gl3.h"
 
+#include "test/TestClearColor.h"
+
 // cd src
 // g++ -std=c++17 -I ./vendor/stb_image/ -I ./vendor/imgui/ ./vendor/imgui/*.cpp ./vendor/stb_image/*.cpp *.cpp -lGLEW -lGLU -lGL -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -w
 // ./a.out
@@ -46,45 +48,45 @@ int main(void)
     // cout << glGetString(GL_VERSION) << endl;
     glewInit();
 
-    float position[] = {
-        -50.0f, -50.0f, 0.0f, 0.0f,
-        50.0f, -50.0f, 1.0f, 0.0f,
-        50.0f, 50.0f, 1.0f, 1.0f,
-        -50.0f, 50.0f, 0.0f, 1.0f};
+    // float position[] = {
+    //     -50.0f, -50.0f, 0.0f, 0.0f,
+    //     50.0f, -50.0f, 1.0f, 0.0f,
+    //     50.0f, 50.0f, 1.0f, 1.0f,
+    //     -50.0f, 50.0f, 0.0f, 1.0f};
 
-    unsigned int indices[] = {
-        0, 1, 2,
-        2, 3, 0};
+    // unsigned int indices[] = {
+    //     0, 1, 2,
+    //     2, 3, 0};
 
-    GLCall(glEnable(GL_BLEND));
-    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+    // GLCall(glEnable(GL_BLEND));
+    // GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-    VertexArray va;
-    VertexBuffer vb(position, 4 * 4 * sizeof(float));
+    // VertexArray va;
+    // VertexBuffer vb(position, 4 * 4 * sizeof(float));
 
-    VertexBufferLayout layout;
-    layout.Push<float>(2);
-    layout.Push<float>(2);
-    va.AddBuffer(vb, layout);
+    // VertexBufferLayout layout;
+    // layout.Push<float>(2);
+    // layout.Push<float>(2);
+    // va.AddBuffer(vb, layout);
 
-    IndexBuffer ib(indices, 6);
+    // IndexBuffer ib(indices, 6);
 
-    glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+    // glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+    // glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
-    Shader shader("../res/shader/basic.shader");
-    shader.Bind();
-    shader.SetUniform4f("u_Color", 1.0f, 0.0f, 1.0f, 1.0f);
+    // Shader shader("../res/shader/basic.shader");
+    // shader.Bind();
+    // shader.SetUniform4f("u_Color", 1.0f, 0.0f, 1.0f, 1.0f);
 
-    Texture texture("../res/textures/ladyonfire.png");
-    texture.Bind();
+    // Texture texture("../res/textures/ladyonfire.png");
+    // texture.Bind();
 
-    shader.SetUniform1i("u_Texture", 0);
+    // shader.SetUniform1i("u_Texture", 0);
 
-    va.Unbind();
-    shader.Unbind();
-    vb.Unbind();
-    ib.Unbind();
+    // va.Unbind();
+    // shader.Unbind();
+    // vb.Unbind();
+    // ib.Unbind();
 
     Renderer renderer;
 
@@ -92,46 +94,51 @@ int main(void)
     ImGui_ImplGlfwGL3_Init(window, true);
     ImGui::StyleColorsDark();
 
-    glm::vec3 translationA(200, 200, 0);
-    glm::vec3 translationB(400, 200, 0);
+    // glm::vec3 translationA(200, 200, 0);
+    // glm::vec3 translationB(400, 200, 0);
 
-    float r = 0.0f;
-    float increment = 0.05f;
+    test::TestClearColor test;
+
+    // float r = 0.0f;
+    // float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         renderer.Clear();
         ImGui_ImplGlfwGL3_NewFrame();
-        shader.Bind();
+        // shader.Bind();
 
-        {
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-            glm::mat4 mvp = proj * view * model;
-            shader.SetUniformMat4f("u_MVP", mvp);
+        // {
+        //     glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+        //     glm::mat4 mvp = proj * view * model;
+        //     shader.SetUniformMat4f("u_MVP", mvp);
             
-            renderer.Draw(va, ib, shader);
-        }
+        //     renderer.Draw(va, ib, shader);
+        // }
 
-        {
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-            glm::mat4 mvp = proj * view * model;
-            shader.SetUniformMat4f("u_MVP", mvp);
+        // {
+        //     glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+        //     glm::mat4 mvp = proj * view * model;
+        //     shader.SetUniformMat4f("u_MVP", mvp);
 
-            renderer.Draw(va, ib, shader);
-        }
+        //     renderer.Draw(va, ib, shader);
+        // }
+        test.OnUpdate(0.0f);
+        test.OnRender();
+        test.onImGuiRender();
 
-        {
-            ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);
-            ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-        }
+        // {
+        //     ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);
+        //     ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
+        //     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        // }
 
-        if (r > 1.0f)
-            increment = -0.05f;
-        else if (r < 0.0f)
-            increment = 0.05f;
-        r += increment;
+        // if (r > 1.0f)
+        //     increment = -0.05f;
+        // else if (r < 0.0f)
+        //     increment = 0.05f;
+        // r += increment;
 
         ImGui::Render();
         ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
